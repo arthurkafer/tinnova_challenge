a02b24f918bd53c2369e120dc69c5336016b9546
arthurkafer <arthurkafer@gmail.com>
2025-06-02 11:35:18 -0300

    adicionado soma dos divisores de 3 e 5

---
+++ src/3_5_mult_sum.py
@@ -0,0 +1,15 @@
+def mult_checker(div):
+    return lambda x: x % div == 0
+
+def sum_of_multiples(list_until):
+    lista = list(range(list_until))
+    check_div_3 = mult_checker(3)
+    check_div_5 = mult_checker(5)
+
+    sum = 0
+    for item in lista:
+        if check_div_3(item) or check_div_5(item):
+            sum+=item
+    return sum
+
+print(sum_of_multiples(10))
